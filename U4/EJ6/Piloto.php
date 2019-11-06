b52047322f49bf80d6156837328d923f99ced163
SandraAP1996 <sandra.empres@gmail.com>
2019-11-06 20:52:47 +0100

    Parte 2

--- U4/EJ6/Piloto.php
+++ U4/EJ6/Piloto.php
@@ -1,6 +1,43 @@
 <?php
 class Piloto{
 
+    //ATRIBUTOS
+    private $nombre;
+    private $dorsal;
+    private $edad;
+
+    //GETTERS & SETTERS
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function getDorsal(){
+        return $this->dorsal;
+    }
+
+    public function setDorsal($dorsal){
+        $this->dorsal = $dorsal;
+    }
+
+    public function getEdad(){
+        return $this->edad;
+    }
+
+    //CONSTRUCTOR
+    public function __construct($nombre,$dorsal,$edad){
+        $this->nombre=$nombre;
+        $this->dorsal=$dorsal;
+        $this->edad=$edad;
+    }
+
+    //METODOS
+    public function __toString(){
+        return "-Nombre: $this->nombre <br> -Dorsal: $this->dorsal <br> -Edad: $this->edad";
+    }
 }
 
 
